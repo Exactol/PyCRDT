c98800d601f7d199af2c4f4157639761996c13b1
Zach <zachmatuson@gmail.com>
2019-12-14 18:20:56 -0500

    Stopped sender thread if no clients connected

--- ServerMode.py
+++
@@ -1,5 +0,0 @@
-from enum import Enum
-
-class ServerMode(Enum):
-    SERVER = 0
-    CLIENT = 1
--- ThreadedServer.py
+++ ThreadedServer.py
@@ -9,10 +9,14 @@ class ThreadedServer:
     def __init__(self, host, port):
         self.host = host
         self.port = port
-        self.running = True
+
+        self.listeners_running = True
+        self.senders_running = False
+
         self.in_queue = Queue()
         self.out_queue = Queue()
 
+        # initial socket setup
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((self.host, self.port))
@@ -26,11 +30,12 @@ class ThreadedServer:
         threading.Thread(target=self._listen).start()
 
     def stop(self):
-        self.running = False
-        self.send("EXIT")
+        print("Shutting down...")
+        self.listeners_running = False
+        self.senders_running = False
 
     def _send(self):
-        while self.running:
+        while self.senders_running:
             try:
                 data = self.out_queue.get(block=True, timeout=1).encode()
                 # send data to each client connected
@@ -44,7 +49,7 @@ class ThreadedServer:
 
     def _listen(self):
         size = 1024
-        while self.running:
+        while self.listeners_running:
             try:
                 # wait for new connections with a timeout of 1 second
                 readable, writable, errored = select.select(self.clients, [], [], 1)
@@ -56,6 +61,12 @@ class ThreadedServer:
 
                         # store client
                         self.clients.append(client)
+
+                        # if sending thread hasnt started yet, start it. will always have atleast 1 client, since server socket is stored in same array
+                        if (not self.senders_running and len( self.clients ) > 1):
+                            print("Starting sender thread")
+                            self.senders_running = True
+                            threading.Thread(target=self._send).start()
 
                         # TODO: update with current state
 
@@ -69,6 +80,11 @@ class ThreadedServer:
                             print(f"{sock[0]}:{sock[1]} disconnected")
                             self.clients.remove(s)
 
+                            # if all clients disconnect, stop sender thread
+                            if (self.senders_running and len( self.clients ) == 1):
+                                print("Stopping sender thread")
+                                self.senders_running = False
+
                         print(data)
 
                 for e in errored:
@@ -76,5 +92,6 @@ class ThreadedServer:
             except Exception as e:
                 print(e)
 
+    # TODO: if no clients connected, out_queue will just keep piling up. maybe dont enqueue values if no clients connected
     def send(self, value):
         self.out_queue.put(value)
